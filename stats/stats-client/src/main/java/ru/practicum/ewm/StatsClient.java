7f2dd782e71acfc19d6ec71dce88bdc2281b229d
Nikolay Aleksandrov <nkl.aleks@yandex.ru>
2026-04-29 11:20:41 +0300

    Spring Cloud infrastructure (#1)
    * refactor: replace main service in core module
    
    * feat: implement discovery, config and gateway services
    
    * feat: connect main with Eureka and config-server
    
    * feat: integrate stats-client with service discovery
    
    * feat: connect stats-server with Eureka and config-server
    
    * refactor: reorder dependencies in stats module pom files
    
    * fix: add sql.init.mode always to main and stats config
    
    * test: actualize Postman test suite

--- stats/stats-client/src/main/java/ru/practicum/ewm/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/StatsClient.java
@@ -1,36 +1,47 @@
 package ru.practicum.ewm;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
 import java.util.List;
 
 @Component
 public class StatsClient {
-    private final RestTemplate rest;
+    private final RestTemplate restTemplate;
+    private final String statsServerUrl;
 
-    @Autowired
-    public StatsClient(RestTemplateBuilder builder, @Value("${ewm.stats-server.url}") String serverUrl) {
-        this.rest = builder
-                .rootUri(serverUrl)
-                .build();
+    public StatsClient(RestTemplate restTemplate,
+                       @Value("${ewm.stats-server.url}") String statsServerUrl) {
+        this.restTemplate = restTemplate;
+        this.statsServerUrl = statsServerUrl;
     }
 
+    @Retryable(
+            retryFor = {RestClientException.class, IOException.class},
+            maxAttempts = 6,
+            backoff = @Backoff(delay = 1000, multiplier = 1.5)
+    )
     public ResponseEntity<HitDto> createHit(CreateHitDto createDto) {
-        HttpEntity<CreateHitDto> requestEntity = new HttpEntity<>(createDto, defaultHeaders());
-        return rest.exchange(
-                "/hit",
-                HttpMethod.POST,
-                requestEntity,
-                HitDto.class);
+            return restTemplate.exchange(
+                    statsServerUrl + "/hit",
+                    HttpMethod.POST,
+                    new HttpEntity<>(createDto, defaultHeaders()),
+                    HitDto.class);
     }
 
+    @Retryable(
+            retryFor = {RestClientException.class, IOException.class},
+            maxAttempts = 6,
+            backoff = @Backoff(delay = 1000, multiplier = 1.5)
+    )
     public ResponseEntity<List<ViewStatsDto>> getStats(String start, String end, List<String> uris, Boolean unique) {
         String uri = UriComponentsBuilder
                 .fromUriString("/stats")
@@ -43,8 +54,8 @@ public class StatsClient {
 
         HttpEntity<Void> requestEntity = new HttpEntity<>(defaultHeaders());
 
-        return rest.exchange(
-                uri,
+        return restTemplate.exchange(
+                statsServerUrl + uri,
                 HttpMethod.GET,
                 requestEntity,
                 new ParameterizedTypeReference<>() {
